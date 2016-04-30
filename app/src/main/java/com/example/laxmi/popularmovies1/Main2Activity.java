ada61ce287bb7d48d859611d205259ac81000ad9
laxmi <ammireddychirla@yahoo.com>
2016-04-30 23:04:19 +0530

    commited

--- app/src/main/java/com/example/laxmi/popularmovies1/Main2Activity.java
+++ app/src/main/java/com/example/laxmi/popularmovies1/Main2Activity.java
@@ -2,11 +2,8 @@ package com.example.laxmi.popularmovies1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,15 +38,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
